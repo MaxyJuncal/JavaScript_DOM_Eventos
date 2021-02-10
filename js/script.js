948e9118827fec501beb910adfb19290943a849a
maxy juncal <maxyjuncal18@gmail.com>
2021-02-09 21:20:57 -0300

    primer commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//querySelector                                            //da un valor de 0 o 1
+const heading = document.querySelector('.header__texto h2')//Elementos.
+// heading.textContent = 'Nuevo Heading';
+console.log(heading);
+
+//querySelectorAll
+
+
+
+//getElementById
